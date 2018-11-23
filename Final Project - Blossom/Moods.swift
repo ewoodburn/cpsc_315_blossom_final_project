38a9fbe94dca5fd5720671e745adc1d731632a9e
arihibb <arianahibbard@outlook.com>
2018-11-23 11:47:09 -0800

    Added a file 'Moods' which defines an enum we can use to record and track a user's mood.

---
+++ Final Project - Blossom/Moods.swift
@@ -0,0 +1,17 @@
+//
+//  Moods.swift
+//  Final Project - Blossom
+//
+//  Created by Ariana Hibbard on 11/23/18.
+//  Copyright © 2018 Emma Woodburn. All rights reserved.
+//
+
+import Foundation
+
+enum Moods: String {
+    case very_good = "Very good"
+    case good = "Good"
+    case moderate = "Moderate"
+    case bad = "Bad"
+    case very_bad = "Very bad"
+}
